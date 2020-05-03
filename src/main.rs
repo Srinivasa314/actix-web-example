d520f71bc59f653c9a315b91a397ee31ba20f3ac
Srinivasa <m.srinivasa1234@gmail.com>
2020-05-03 16:55:30 +0530

    Created login page

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use actix_web::{App, HttpServer};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn login() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../static/login.html"))
+}
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new())
+    HttpServer::new(|| App::new().service(login))
         .bind("127.0.0.1:8000")?
         .run()
         .await
