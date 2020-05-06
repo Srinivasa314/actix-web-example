5ce3e4fbf2a82c64d0811cfde7f012b61e79f159
Srinivasa <m.srinivasa1234@gmail.com>
2020-05-06 13:14:23 +0530

    Implemented welcome page and logout

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 use actix_files::NamedFile;
-use actix_web::{get, post, web::block, web::Data, web::Form, App, HttpServer, Responder};
+use actix_web::{
+    error, get, http, post, web::block, web::Data, web::Form, App, HttpResponse, HttpServer,
+    Responder, Result,
+};
 
 #[macro_use]
 extern crate diesel;
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
 use diesel::{insert_into, MysqlConnection, RunQueryDsl};
+
+use actix_identity::{CookieIdentityPolicy, Identity, IdentityService};
+use io::prelude::*;
 use schema::accounts::dsl::{self, accounts};
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
+use std::{fs, io};
+use tera::Tera;
 
 #[get("/style.css")]
 async fn style() -> impl Responder {
@@ -16,6 +24,22 @@ async fn style() -> impl Responder {
 }
 
 #[get("/")]
+async fn index(tmpl: Data<Tera>, id: Identity) -> Result<HttpResponse> {
+    if id.identity().is_some() {
+        let mut ctx = tera::Context::new();
+        ctx.insert("name", &id.identity().unwrap());
+        Ok(HttpResponse::Ok().content_type("text/html").body(
+            tmpl.render("welcome.html", &ctx)
+                .map_err(|_| error::ErrorInternalServerError("Template error"))?,
+        ))
+    } else {
+        Ok(HttpResponse::Found()
+            .header(http::header::LOCATION, "/login")
+            .finish())
+    }
+}
+
+#[get("/login")]
 async fn login() -> impl Responder {
     NamedFile::open("static/login.html")
 }
@@ -23,6 +47,12 @@ async fn login() -> impl Responder {
 #[get("/newacc")]
 async fn newacc() -> impl Responder {
     NamedFile::open("static/newacc.html")
+}
+
+#[get("/logout")]
+async fn logout(id: Identity) -> impl Responder {
+    id.forget();
+    NamedFile::open("static/login.html")
 }
 
 fn sha256(s: &str) -> Vec<u8> {
@@ -63,7 +93,11 @@ async fn create_account(pool: Data<DbPool>, params: Form<FormParams>) -> impl Re
 }
 
 #[post("/login_request")]
-async fn login_request(pool: Data<DbPool>, params: Form<FormParams>) -> impl Responder {
+async fn login_request(
+    id: Identity,
+    pool: Data<DbPool>,
+    params: Form<FormParams>,
+) -> impl Responder {
     let conn = pool.get().expect("Could not get db connection");
     let username = params.username.clone();
 
@@ -78,6 +112,7 @@ async fn login_request(pool: Data<DbPool>, params: Form<FormParams>) -> impl Res
             0 => Ok("No such user"),
             _ => {
                 if result[0].password_hash == sha256(&params.password) {
+                    id.remember(params.username.clone());
                     Ok("LOGIN_SUCCESS")
                 } else {
                     Ok("Wrong Password")
@@ -90,22 +125,46 @@ async fn login_request(pool: Data<DbPool>, params: Form<FormParams>) -> impl Res
 
 #[actix_rt::main]
 
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        dotenv::dotenv().ok();
-        let conn_url = std::env::var("DATABASE_URL").expect("Failed to get value of DATABASE_URL");
-        let manager = ConnectionManager::<MysqlConnection>::new(conn_url);
+async fn main() -> io::Result<()> {
+    dotenv::dotenv().ok();
+    let conn_url = std::env::var("DATABASE_URL").expect("Failed to get value of DATABASE_URL");
+    let private_key = match fs::read("cookie-key") {
+        Ok(bytes) => bytes,
+        Err(e) => {
+            if e.kind() == io::ErrorKind::NotFound {
+                let mut f =
+                    fs::File::create("cookie-key").expect("Unable to create cookie key file");
+                let key: [u8; 32] = rand::random();
+                f.write(&key).expect("Unable to write to file");
+                key.to_vec()
+            } else {
+                panic!(e)
+            }
+        }
+    };
 
+    HttpServer::new(move || {
+        let manager = ConnectionManager::<MysqlConnection>::new(&conn_url);
         let pool = r2d2::Pool::builder()
             .build(manager)
             .expect("Failed to create pool");
+        let tera = Tera::new("templates/*.html").expect("Failed to parse template files");
 
         App::new()
+            .wrap(IdentityService::new(
+                CookieIdentityPolicy::new(&private_key)
+                    .name("actix-web-example")
+                    .secure(false)
+                    .max_age(31556952),
+            ))
             .data(pool)
-            .service(login)
+            .data(tera)
+            .service(index)
             .service(newacc)
+            .service(login)
             .service(create_account)
             .service(style)
+            .service(logout)
             .service(login_request)
     })
     .bind("127.0.0.1:8000")?
